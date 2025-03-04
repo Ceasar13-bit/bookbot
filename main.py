ea11bf65988a49b5fe52271abd913ffc8d08160b
Ceasar13-bit <cezary.gajewski13@gmail.com>
2025-03-04 22:41:53 +0100

    Add character counting

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-from stats import count_words
+import sys
+from stats import count_words, count_characters, sort_result, create_report
 
 def main():
-       print(count_words(get_book_text("books/frankenstein.txt")))
+       if len(sys.argv) != 2:
+              print("Usage: python3 main.py <path_to_book>")
+              sys.exit(1)
+       book_path = sys.argv[1]
+       book = get_book_text(book_path)
+       words = count_words(book)
+       characters = sort_result(count_characters(book))
+       create_report(characters,words,book_path)
 
 def get_book_text(path_to_book):
-    with open(path_to_book) as file:
-              file_contens = file.read()
-              return file_contens
-
+       try:
+              with open(path_to_book) as file:
+                     file_contens = file.read()
+                     return file_contens
+       except FileNotFoundError:
+              print("No book found")
+              sys.exit(2)
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,36 @@
+import string
+
 def count_words(book):
-       return f"{len(book.split())} words found in the document"
+       return f"Found {len(book.split())} total words"
+
+def count_characters(book):
+       book = book.lower()
+       characters = string.printable + "æâêëô" #not needed
+       result = {}
+       for char in book:
+              if char in characters:  #not needed
+                     try:
+                            result[char] += 1
+                     except KeyError:
+                            result[char] = 1
+              
+       return result
+
+def sort_result(chars):
+       report = []
+       for char in chars:
+              if char.isalpha():  #move to upper function
+                     report.append({"character": char, "count": chars[char]})
+       report.sort(key=lambda character: character["count"], reverse=True)
+       return report
+
+def create_report(report, result, book_path):
+       print("============ BOOKBOT ============")
+       print(f"Analyzing book found at {book_path}")
+       print("----------- Word Count ----------")
+       print(result)
+       print("--------- Character Count -------")
+       for char in report:
+              print(f"{char["character"]}: {char["count"]}")
+       print("============= END ===============")
+
