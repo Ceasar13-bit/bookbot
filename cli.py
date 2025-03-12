516bfd25dd04c3523e5b738ec9617f095bb4d53d
Ceasar13-bit <cezary.gajewski13@gmail.com>
2025-03-12 21:44:37 +0100

    Removed unnecessary lines

--- cli.py
+++ cli.py
@@ -2,6 +2,6 @@ import argparse
 
 def get_args():
     parser = argparse.ArgumentParser(description="Bookbot - tool for text fiels analizing")
-    parser.add_argument("file", type=str, help="path to the text file")
+    parser.add_argument("file_path", type=str, help="path to the text file")
 
     return parser.parse_args()
--- main.py
+++ main.py
@@ -5,10 +5,7 @@ from cli import get_args
 
 def main():
        args = get_args()
-       if len(sys.argv) != 2:
-              print("Usage: python3 main.py <path_to_book>")
-              sys.exit(1)
-       book_path = sys.argv[1]
+       book_path = args.file_path
        book = get_book_text(book_path)
        words = count_words(book)
        characters = sort_result(count_characters(book))
